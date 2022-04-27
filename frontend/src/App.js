bd97a6626f41c2514a01dfc2b9797c218197a3b0
Doyinsayo Otufadebo <64381567+doyinsayo@users.noreply.github.com>
2022-04-27 22:37:49 +0100

    reducers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,27 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { connect } from "react-redux";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { connect,Provider } from "react-redux";
 import BaseRouter from "./routes";
 import * as actions from "./store/actions/auth";
 import "semantic-ui-css/semantic.min.css";
 import CustomLayout from "./containers/Layout";
 import Login from "./containers/Login";
+import Home from './containers/Home'
+import Signup from "./containers/Signup";
+import store from "./store";
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onTryAutoSignup();
-  }
-
-  render() {
-    return (
+const App = () => (
+  <Provider store={store} >
       <Router>
-        <CustomLayout {...this.props}>
-          <BaseRouter />
-          <Login />
-        </CustomLayout>
+          <CustomLayout>
+              <Routes>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/login' component={Login} />
+                  <Route exact path='/signup' component={Signup} />
+              </Routes>
+          </CustomLayout>
       </Router>
-    );
-  }
-}
+  </Provider>
+);
 
-const mapStateToProps = state => {
-  return {
-    isAuthenticated: state.auth.token !== null
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onTryAutoSignup: () => dispatch(actions.authCheckState())
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default App;
---
+++ frontend/src/store.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, combineReducers} from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from 'redux-thunk'
+import rootReducer from './store/reducers/auth';
+
+const initialState = {};
+
+const middleware = [thunk]
+
+const store = configureStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
