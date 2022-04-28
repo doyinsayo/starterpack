d79855344686bceeead89d9cc995143df34db784
Doyinsayo Otufadebo <64381567+doyinsayo@users.noreply.github.com>
2022-04-28 16:05:41 +0100

    ..

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,12 @@
-import { applyMiddleware, combineReducers, compose} from "redux";
+import { combineReducers} from "redux";
 import { configureStore } from "@reduxjs/toolkit";
-import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from 'redux-thunk'
 import authReducer from './store/reducers/auth'
-
-const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 
 const rootReducer = combineReducers({
     auth: authReducer
   });
 
-const store = configureStore(rootReducer, composeEnhances(applyMiddleware(thunk)));
+const store = configureStore({reducer:rootReducer, middleware:[thunk]});
 
 export default store
